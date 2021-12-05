41b457920ccfbfeb74582a23cb6750e9eacdb414
Thomas Wiss <thomas.wiss@neotechnology.com>
2021-12-05 12:53:09 +0100

    mobile enabled

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from "react";
-import { Layout } from "antd";
+import React, { FC, useState } from "react";
+import { Button, Layout } from "antd";
+import { LayoutOutlined } from "@ant-design/icons";
 import { openRedirectModal } from "../modal/Modals";
 import Stepper from "../stepper/Stepper";
-// import Viewer from "../viewer/Viewer";
+import Viewer from "../viewer/Viewer";
 import "./Main.css";
 
 const { Header, Footer, Content } = Layout;
@@ -14,21 +15,35 @@ const doRedirect = () => {
   });
 };
 
-const Main: FC = () => (
-  <>
-    <Layout className="layout">
-      <Header className="header">
-        Discuss Project Management fundamental concepts
-      </Header>
-      <Content className="content">
-        <Stepper onCompleted={doRedirect} />
-        {/* <Viewer onCompleted={doRedirect} /> */}
-      </Content>
-      <Footer>
-        Copyright of content: xxx TM; Just in case: Link to the survey
-      </Footer>
-    </Layout>
-  </>
-);
+const Main: FC = () => {
+  const [isStepper, setIsStepper] = useState(true);
+
+  return (
+    <>
+      <Layout className="layout">
+        <Header className="header">
+          Discuss Project Management fundamental concepts
+          <Button
+            style={{ marginLeft: "1em" }}
+            type="link"
+            onClick={() => setIsStepper(!isStepper)}
+          >
+            <LayoutOutlined />
+          </Button>
+        </Header>
+        <Content className="content">
+          {isStepper ? (
+            <Stepper onCompleted={doRedirect} />
+          ) : (
+            <Viewer onCompleted={doRedirect} />
+          )}
+        </Content>
+        <Footer>
+          Copyright of content: xxx TM; Just in case: Link to the survey
+        </Footer>
+      </Layout>
+    </>
+  );
+};
 
 export default Main;
