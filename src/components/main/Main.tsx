515a40422a4bb0c2bbf3fabcddfb3f9380496aa6
Thomas Wiss <thomas.wiss@neotechnology.com>
2021-12-26 14:49:08 +0100

    primitive early svg load

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,8 +3,12 @@ import { Layout } from "antd";
 import { openRedirectModal } from "../modal/Modals";
 import Stepper from "../stepper/Stepper";
 import Viewer from "../viewer/Viewer";
-import "./Main.css";
 import LandingPage from "../landing/LandingPage";
+import Spark from "../../assets/celebration-spark.svg";
+import Garland from "../../assets/celebration-garland.svg";
+import Medal from "../../assets/medal-1369.svg";
+import Quality from "../../assets/quality-3602.svg";
+import "./Main.css";
 
 const { Header, Footer, Content } = Layout;
 
@@ -43,6 +47,13 @@ const Main: FC = () => {
               )}
             </>
           )}
+          <div style={{ visibility: "hidden" }}>
+            {/* To load the svg's on page load */}
+            <img src={Spark} alt="Spark" width="10px" height="10px" />
+            <img src={Garland} alt="Garland" width="10px" height="10px" />
+            <img src={Medal} alt="Medal" width="10px" height="10px" />
+            <img src={Quality} alt="Quality" width="10px" height="10px" />
+          </div>
         </Content>
         <Footer onClick={() => setIsLandingPage(true)}>
           Copyright of content: xxx TM; Just in case: Link to the survey
